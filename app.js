8bac75b9764eca6123faf4370cdb5c0c4d86edad
Eddy Caldas <eddycaldas@live.com>
2018-01-08 12:25:59 -0700

    dom basics

--- app.js
+++ app.js
@@ -15,5 +15,27 @@ val = document.forms[0];
 val = document.forms[0].id;
 val = document.forms[0].method;
 
+val = document.links;
+val = document.links[0];
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
+
+
+// forEach only work arrays only so convert into array tqake the scripts create a variable and pass the html collection
+
+let scripts = document.scripts;
+
+let scriptsArr = Array.from(scripts);
+
+scriptsArr.forEach(function(script) {
+    console.log(script.getAttribute('src'))
+});
+
+
 
 console.log(val);
