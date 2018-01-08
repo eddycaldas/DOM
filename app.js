aee91e53b16d37c2b8e9f520d270a0c298773b5b
Eddy Caldas <eddycaldas@live.com>
2018-01-08 12:12:50 -0700

    DOM intro

---
+++ app.js
@@ -0,0 +1,19 @@
+let val;
+
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype; 
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+
+
+console.log(val);
